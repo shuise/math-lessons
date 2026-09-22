227cdcf514a1ef6e783a8989ec631b16d3d14e76
shuise <riverrunning@tutamail.com>
2026-09-22 11:30:30 +0800

    feat: 专题

---
+++ specials/dt.js
@@ -0,0 +1,8 @@
+// 百度统计
+(function() {
+    window._hmt = window._hmt || [];
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?4f2d8402fb6b0cb7227ec1d03b750df7";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
